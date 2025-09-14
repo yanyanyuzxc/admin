76f6348574ef2bb33efcd94be51bf3ce89bfa3c4
杨磊 <15915115+yanyanyuzxc@user.noreply.gitee.com>
2025-09-14 12:05:05 +0800

    second

---
+++ admin/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+const fn = () => {
+
+
+    
+    console.log('Hello, World!')
+}
+
+
+
+fn()
